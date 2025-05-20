8604f6b42f17828cd013654eca6a4eb3a7f6cef6
ankitverma3490 <ankitverma3490@gmail.com>
2025-05-20 18:20:13 +0530

    wip

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "http://192.168.1.46:4000/api";
 // const BASE_URL = "https://8l11vvs0-3000.inc1.devtunnels.ms/api";
 
 export default BASE_URL
