48ad105df1eeaccd3f4ca7789b080b1e0b73c212
krishnarajput <161302830+KrishnpalJadam@users.noreply.github.com>
2025-05-31 12:44:00 +0530

    change

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://192.168.1.46:4000/api";
+// const BASE_URL = "http://192.168.1.46:4000/api";
 // const BASE_URL = "https://8l11vvs0-3000.inc1.devtunnels.ms/api";
-// const  BASE_URL = "https://aiskillsproject-production.up.railway.app/api"
+const  BASE_URL = "https://aiskillsproject-production.up.railway.app/api"
 export default BASE_URL
