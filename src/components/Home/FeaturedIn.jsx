95a995681b8de5577537b488a93ba515936dd608
ap8114 <amanpatidar086@gmail.com>
2025-07-14 14:39:02 +0530

    pull

--- src/components/Home/FeaturedIn.jsx
+++ src/components/Home/FeaturedIn.jsx
@@ -39,24 +39,33 @@ const FeaturedInSection = () => {
       <div className="max-w-[1366px] mx-auto grid grid-cols-1 sm:grid-cols-2 gap-10 items-center">
         {/* Left Section - Heading & Description */}
         <div className="w-full lg:w-auto text-[#ffffff] sm:translate-x-6 transition-all duration-300 sm:text-center  md:text-center lg:text-left">
-          <h2 className="font-[400] text-[40px] sm:text-[55px] leading-[50px] sm:leading-[65px] tracking-[0.5%] mb-4 font-impact">
+          {/* <h2 className="font-[400] text-[40px] sm:text-[55px] leading-[50px] sm:leading-[65px] tracking-[0.5%] mb-4 font-impact">
             FEATURED IN
+          </h2> */}
+          <h2 className="font-[400] text-[40px] sm:text-[55px] leading-[50px] sm:leading-[65px] tracking-[0.5%] mb-4 font-impact">
+           LAUNCH NOW
           </h2>
-          <p className="text-[14px] sm:text-[16px] leading-[22px] sm:leading-[25px] tracking-[0.5%] font-Roboto Condensed">
+          {/* <p className="text-[14px] sm:text-[16px] leading-[22px] sm:leading-[25px] tracking-[0.5%] font-Roboto Condensed">
             ENHANCE YOUR LEARNING WITH INSTANT DIGITAL PRODUCTS
             <br />
             FROM INSTANT REAL-WORLD PROJECTS TO PROMPTS.
+          </p> */}
+            <p className="text-[14px] sm:text-[16px] leading-[22px] sm:leading-[25px] tracking-[0.5%] font-Roboto Condensed">
+           FOR NON COMPUTER SCIENCE MASTERS AND PH.Ds
           </p>
         </div>
 
         {/* Right Section - Logos */}
-        <div className="w-[823] h-[117] sm:-translate-x-6 transition-all duration-300 flex justify-center sm:justify-end">
+        {/* <div className="w-[823] h-[117] sm:-translate-x-6 transition-all duration-300 flex justify-center sm:justify-end">
           <img
             src={featureicon}
             alt="Featured In"
             className="max-w-full h-auto"
           />
-        </div>
+        </div> */}
+        <p className="text-light">A UNIQUE JOB GUARANTEE PLATFORM
+TO LAUNCH YOUR CAREER AS AI/ML PRODUCT MANAGER AND DATA SCIENTIST WITH NO
+UPFRONT FEES. PEOPLE WITH DOMAIN KNOWLEDGE & AI SKILLS WILL RULE THE WORLD.</p>
       </div>
     </div>
   );
